479d3f16e6dc8b9316ef3a4e68da8029984fb159
adamwolfe2 <157528621+adamwolfe2@users.noreply.github.com>
2026-02-17 20:00:12 -0800

    fix: allow build without DATABASE_URL

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -2,9 +2,7 @@ import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
 
-if (!process.env.DATABASE_URL) {
-    throw new Error('DATABASE_URL is not defined');
-}
+const connectionString = process.env.DATABASE_URL || "postgres://user:pass@host:5432/db";
 
-const sql = neon(process.env.DATABASE_URL);
+const sql = neon(connectionString);
 export const db = drizzle(sql, { schema });
